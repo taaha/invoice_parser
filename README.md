2f4595ef08ce8e4673e58f63a3db0e354993c087
darth <taaha.s.bajwa@gmail.com>
2024-02-23 16:57:34 +0500

    removed bugs etc

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# invoice_parser
+# Invoice_Buddy
+
+Steps to run
+1. first install dependencies by `pip install -r requirements.txt`
+2. Now run the app by `streamlit run app.py`
+3. In order to view eval dashboard of truelens, type `truelens-eval`
--- utils.py
+++ utils.py
@@ -5,8 +5,20 @@ import os
 import shutil
 from dotenv import load_dotenv
 import ast
+from trulens_eval import TruCustomApp
+from trulens_eval import Tru
+from trulens_eval.tru_custom_app import instrument
+from trulens_eval import Provider
+from trulens_eval import Feedback
+from trulens_eval import Select
 
 load_dotenv()  # take environment variables from .env.
+
+openai_mm_llm = OpenAIMultiModal(
+        model="gpt-4-vision-preview", api_key=os.environ["OPENAI_API_KEY"], max_new_tokens=300
+    )
+
+tru = Tru()
 
 def empty_directory(directory):
     # Check if the directory exists
@@ -43,6 +55,7 @@ def string_to_dictionary(input_string):
         print(f"Error converting string to dictionary: {e}")
         return None
 
+
 def pass_to_openai_vision_api_llama_index(image):
     gpt_prompt='''Above is the text extracted from an invoice.
 You are an assistant tasked with extracting information from the invoice. Do this step by step.
@@ -70,16 +83,44 @@ Output should only contain a dictionary in the following format
 If a key is not mentioned in invoice or you dont understand, then make its value None
 '''
 
-    openai_mm_llm = OpenAIMultiModal(
-        model="gpt-4-vision-preview", api_key=os.environ["OPENAI_API_KEY"], max_new_tokens=300
-    )
-
     image_documents = SimpleDirectoryReader(input_files=[f"data/{image}"]).load_data()
 
-    response = openai_mm_llm.complete(
-        prompt=gpt_prompt,
-        image_documents=image_documents,
-    )
+    class GPTVision:
+        @instrument
+        def complete(self, prompt, image_documents):
+            completion = openai_mm_llm.complete(
+                prompt=prompt,
+                image_documents=image_documents,
+            )
+            return completion
+
+    gptvision = GPTVision()
+
+    # create a custom gemini feedback provider
+    class GPTVision_Provider(Provider):
+        def bill_rating(self, image_documents) -> float:
+            bill_score = float(openai_mm_llm.complete
+                               (prompt="Does this image have text? Only Respond with the float likelihood from 0.0 (not text) to 1.0 (text).",
+                                image_documents=image_documents).text)
+            return bill_score
+
+    gptvision_provider = GPTVision_Provider()
+
+    f_custom_function = Feedback(gptvision_provider.bill_rating, name="Bill Likelihood").on(
+        Select.Record.calls[0].args.image_documents[0].image_url)
+
+    print(gptvision_provider.bill_rating(image_documents))
+
+    from trulens_eval import TruCustomApp
+    # removing feedback in production to avoid additional API cost. Only using prompt tracking in production
+    # tru_gptvision = TruCustomApp(gptvision, app_id="gptvision1", feedbacks=[f_custom_function])
+    tru_gptvision = TruCustomApp(gptvision, app_id="gptvision1")
+
+    with tru_gptvision as recording:
+        response = gptvision.complete(
+            prompt=gpt_prompt,
+            image_documents=image_documents
+        )
 
     response_dict = string_to_dictionary(response.text)
 
